fb882ac80fec06384c44416bb91e3c681ab421e6
sameer08055 <85116976+sameer08055@users.noreply.github.com>
2026-09-22 18:26:02 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Sam"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
